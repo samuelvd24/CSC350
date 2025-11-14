c4e13415e21455d57f8c22e0c9f007a1f8ebfeff
samuelvd24 <128221108+samuelvd24@users.noreply.github.com>
2025-11-13 20:36:14 -0500

    update
    update

---
+++ public/signup.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <title>QRS Car Rental</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><h2>QRS Car Rentals</h2></a>
+        <nav>
+            <ul>
+                <li><a href="browse.php">Browse</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="signup-main">
+        <img class="signup-main-img" src="image/mercedes.jpg" alt="Mercedes bumper">
+
+    </main>
+    <footer>
+
+        <div class="footer-contact">
+            <div class="footer-container">
+                <h5>CONTACT</h5>
+                <p><a href="">Example@email.com</a></p>
+            </div>
+            <div class="footer-container">
+                <h5>FOLLOW</h5>
+                <div class="footer-social-media">
+                    <a href=""><img class="footer-icon" src="image/instagram-brands-solid.svg" alt="instagram icon"></a>
+                    <a href=""><img class="footer-icon" src="image/twitter-brands-solid.svg" alt="twitter icon"></a>
+                    
+                </div>
+            </div>
+        </div>
+
+        <p class="footer-p">&copy;2025 QRS Rental System. All rights reserved</p>
+    </footer>
+</body>
+</html>
